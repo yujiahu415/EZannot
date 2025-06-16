edf00bf2d87fc6abe889e622320ae356daede630
Yujia Hu <yujiahu415@gmail.com>
2025-06-16 16:27:56 -0400

    Fix SAM2 importing errors

--- EZannot/sam2/sam2_image_predictor.py
+++ EZannot/sam2/sam2_image_predictor.py
@@ -12,9 +12,9 @@ import numpy as np
 import torch
 from PIL.Image import Image
 
-from sam2.modeling.sam2_base import SAM2Base
+from EZannot.sam2.modeling.sam2_base import SAM2Base
 
-from sam2.utils.transforms import SAM2Transforms
+from EZannot.sam2.utils.transforms import SAM2Transforms
 
 
 class SAM2ImagePredictor:
