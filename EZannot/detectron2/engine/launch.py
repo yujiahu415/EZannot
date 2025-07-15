26fcf0bc8e0c846a5dc64671e9e3254a98d04cee
Yujia Hu <yujiahu415@gmail.com>
2025-07-15 16:57:39 -0400

    Update launch.py

--- EZannot/detectron2/engine/launch.py
+++ EZannot/detectron2/engine/launch.py
@@ -5,7 +5,7 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
-from Cellan.detectron2.utils import comm
+from EZannot.detectron2.utils import comm
 
 __all__ = ["DEFAULT_TIMEOUT", "launch"]
 
