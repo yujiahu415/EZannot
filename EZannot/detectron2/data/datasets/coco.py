359ca233d44b10d163cdf5dd326a804e528d2a36
Yujia Hu <yujiahu415@gmail.com>
2025-07-15 16:49:00 -0400

    Update coco.py

--- EZannot/detectron2/data/datasets/coco.py
+++ EZannot/detectron2/data/datasets/coco.py
@@ -12,8 +12,8 @@ from fvcore.common.timer import Timer
 from iopath.common.file_io import file_lock
 from PIL import Image
 
-from Cellan.detectron2.structures import Boxes, BoxMode, PolygonMasks, RotatedBoxes
-from Cellan.detectron2.utils.file_io import PathManager
+from EZannot.detectron2.structures import Boxes, BoxMode, PolygonMasks, RotatedBoxes
+from EZannot.detectron2.utils.file_io import PathManager
 
 from .. import DatasetCatalog, MetadataCatalog
 
@@ -531,9 +531,9 @@ def main() -> None:
     """
     import sys
 
-    import Cellan.detectron2.data.datasets  # noqa  # add pre-defined metadata
-    from Cellan.detectron2.utils.logger import setup_logger
-    from Cellan.detectron2.utils.visualizer import Visualizer
+    import EZannot.detectron2.data.datasets  # noqa  # add pre-defined metadata
+    from EZannot.detectron2.utils.logger import setup_logger
+    from EZannot.detectron2.utils.visualizer import Visualizer
 
     logger = setup_logger(name=__name__)
     assert sys.argv[3] in DatasetCatalog.list()
