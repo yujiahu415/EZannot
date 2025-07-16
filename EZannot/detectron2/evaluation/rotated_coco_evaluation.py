392e24f6cee3a526cac6f34ee233b90553cfca25
Yujia Hu <yujiahu415@gmail.com>
2025-07-16 15:07:36 -0400

    Update rotated_coco_evaluation.py

--- EZannot/detectron2/evaluation/rotated_coco_evaluation.py
+++ EZannot/detectron2/evaluation/rotated_coco_evaluation.py
@@ -6,8 +6,8 @@ import os
 import torch
 from pycocotools.cocoeval import COCOeval, maskUtils
 
-from Cellan.detectron2.structures import BoxMode, RotatedBoxes, pairwise_iou_rotated
-from Cellan.detectron2.utils.file_io import PathManager
+from EZannot.detectron2.structures import BoxMode, RotatedBoxes, pairwise_iou_rotated
+from EZannot.detectron2.utils.file_io import PathManager
 
 from .coco_evaluation import COCOEvaluator
 
