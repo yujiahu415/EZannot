cd86a1c066d1617af5774af8c13c80cccc1789fb
Yujia Hu <yujiahu415@gmail.com>
2025-07-16 15:11:21 -0400

    Update flatten.py

--- EZannot/detectron2/export/flatten.py
+++ EZannot/detectron2/export/flatten.py
@@ -5,8 +5,8 @@ from typing import Callable, List, Optional, Tuple
 import torch
 from torch import nn
 
-from Cellan.detectron2.structures import Boxes, Instances, ROIMasks
-from Cellan.detectron2.utils.registry import _convert_target_to_string, locate
+from EZannot.detectron2.structures import Boxes, Instances, ROIMasks
+from EZannot.detectron2.utils.registry import _convert_target_to_string, locate
 
 from .torchscript_patch import patch_builtin_len
 
