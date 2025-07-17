8144678908e3a3c3467829c6c925d148a79a14a2
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 11:31:42 -0400

    Update backbone.py

--- EZannot/detectron2/modeling/backbone/backbone.py
+++ EZannot/detectron2/modeling/backbone/backbone.py
@@ -3,7 +3,7 @@ from abc import ABCMeta, abstractmethod
 from typing import Dict
 import torch.nn as nn
 
-from Cellan.detectron2.layers import ShapeSpec
+from EZannot.detectron2.layers import ShapeSpec
 
 __all__ = ["Backbone"]
 
