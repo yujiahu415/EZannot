4959bed838aec94381cf0f5b45d2449ac9276e44
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 11:59:55 -0400

    Update postprocessing.py

--- EZannot/detectron2/modeling/postprocessing.py
+++ EZannot/detectron2/modeling/postprocessing.py
@@ -2,7 +2,7 @@
 import torch
 from torch.nn import functional as F
 
-from Cellan.detectron2.structures import Instances, ROIMasks
+from EZannot.detectron2.structures import Instances, ROIMasks
 
 
 # perhaps should rename to "resize_instance"
