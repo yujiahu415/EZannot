05163a572a84947277e71fa5c5095a5432fa5c0a
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 12:17:58 -0400

    Update utils.py

--- EZannot/detectron2/tracking/utils.py
+++ EZannot/detectron2/tracking/utils.py
@@ -2,7 +2,7 @@
 import numpy as np
 from typing import List
 
-from Cellan.detectron2.structures import Instances
+from EZannot.detectron2.structures import Instances
 
 
 def create_prediction_pairs(
