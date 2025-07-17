1fdde80590efd75b8cdde01425bf021827a93e36
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 12:17:05 -0400

    Update base_tracker.py

--- EZannot/detectron2/tracking/base_tracker.py
+++ EZannot/detectron2/tracking/base_tracker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2004-present Facebook. All Rights Reserved.
-from Cellan.detectron2.config import configurable
-from Cellan.detectron2.utils.registry import Registry
+from EZannot.detectron2.config import configurable
+from EZannot.detectron2.utils.registry import Registry
 
 from ..config.config import CfgNode as CfgNode_
 from ..structures import Instances
