b9c2a806e66c4d7db70ec062a1e21712f0958168
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 12:10:06 -0400

    Update roi_heads.py

--- EZannot/detectron2/projects/point_rend/roi_heads.py
+++ EZannot/detectron2/projects/point_rend/roi_heads.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import logging
 
-from Cellan.detectron2.modeling import ROI_HEADS_REGISTRY, StandardROIHeads
+from EZannot.detectron2.modeling import ROI_HEADS_REGISTRY, StandardROIHeads
 
 
 @ROI_HEADS_REGISTRY.register()
