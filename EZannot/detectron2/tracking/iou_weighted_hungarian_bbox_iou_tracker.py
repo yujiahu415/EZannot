80969afe5247c07c6f877b46a6ac05407ba59d8b
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 12:17:46 -0400

    Update iou_weighted_hungarian_bbox_iou_tracker.py

--- EZannot/detectron2/tracking/iou_weighted_hungarian_bbox_iou_tracker.py
+++ EZannot/detectron2/tracking/iou_weighted_hungarian_bbox_iou_tracker.py
@@ -4,8 +4,8 @@
 import numpy as np
 from typing import List
 
-from Cellan.detectron2.config import CfgNode as CfgNode_
-from Cellan.detectron2.config import configurable
+from EZannot.detectron2.config import CfgNode as CfgNode_
+from EZannot.detectron2.config import configurable
 
 from .base_tracker import TRACKER_HEADS_REGISTRY
 from .vanilla_hungarian_bbox_iou_tracker import VanillaHungarianBBoxIOUTracker
@@ -76,7 +76,7 @@ class IOUWeightedHungarianBBoxIOUTracker(VanillaHungarianBBoxIOUTracker):
         min_instance_period = cfg.TRACKER_HEADS.get("MIN_INSTANCE_PERIOD", 1)
         track_iou_threshold = cfg.TRACKER_HEADS.get("TRACK_IOU_THRESHOLD", 0.5)
         return {
-            "_target_": "Cellan.detectron2.tracking.iou_weighted_hungarian_bbox_iou_tracker.IOUWeightedHungarianBBoxIOUTracker",  # noqa
+            "_target_": "EZannot.detectron2.tracking.iou_weighted_hungarian_bbox_iou_tracker.IOUWeightedHungarianBBoxIOUTracker",  # noqa
             "video_height": video_height,
             "video_width": video_width,
             "max_num_instances": max_num_instances,
