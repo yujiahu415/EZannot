7d2009d49e7b539d0bbc821c5dd99fb95fcbbddc
Yujia Hu <yujiahu415@gmail.com>
2025-07-17 12:19:28 -0400

    Update env.py

--- EZannot/detectron2/utils/env.py
+++ EZannot/detectron2/utils/env.py
@@ -123,7 +123,7 @@ def setup_custom_environment(custom_module):
     module, and run the setup function.
     """
     if custom_module.endswith(".py"):
-        module = _import_file("Cellan.detectron2.utils.env.custom_module", custom_module)
+        module = _import_file("EZannot.detectron2.utils.env.custom_module", custom_module)
     else:
         module = importlib.import_module(custom_module)
     assert hasattr(module, "setup_environment") and callable(module.setup_environment), (
