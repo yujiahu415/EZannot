c34e70f691cb8b2e8e382a2009d14d7ea1aa815d
Yujia Hu <yujiahu415@gmail.com>
2025-10-22 15:30:31 -0400

    Update gui.py

--- EZannot/gui.py
+++ EZannot/gui.py
@@ -20,7 +20,7 @@ from EZannot.sam2.build_sam import build_sam2
 from EZannot.sam2.sam2_image_predictor import SAM2ImagePredictor
 from EZannot import __version__
 from .annotator import Annotator,AutoAnnotation
-from .tools import read_annotation
+from .tools import read_annotation,generate_annotation,measure_annotation
 
 
 
