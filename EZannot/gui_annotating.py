ffdd83bcc04d013fa88f08a3b75e577cfbd56d06
Yujia Hu <yujiahu415@gmail.com>
2025-11-04 10:02:26 -0500

    Update gui_annotating.py

--- EZannot/gui_annotating.py
+++ EZannot/gui_annotating.py
@@ -364,7 +364,7 @@ class WindowLv3_AnnotateImages(wx.Frame):
 		self.current_segmentation=None
 		self.current_polygon=[]
 		self.current_classname=list(self.color_map.keys())[0]
-		self.information=read_annotation(os.path.dirname(self.image_paths[0]))
+		self.information=read_annotation(os.path.dirname(self.image_paths[0]),color_map=self.color_map)
 		self.foreground_points=[]
 		self.background_points=[]
 		self.selected_point=None
